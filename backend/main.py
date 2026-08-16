c413cc25e3700b965bc9e3d1f635356eb0764006
SergioTolaba26 <esete2526@gmail.com>
2026-08-16 20:25:03 -0300

    Configurar CORS para frontend

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from presentation.routers.empresa_router import (
     router as empresa_router,
 )
 from presentation.routers.cuenta_router import (
     router as cuenta_router,
 )
-
 from presentation.routers.libro_diario_router import (
     router as libro_diario_router,
 )
-
 from presentation.routers.libro_mayor_router import (
     router as libro_mayor_router,
 )
@@ -25,22 +25,24 @@ from presentation.routers.estado_resultados_router import (
 from presentation.routers.ejercicio_router import (
     router as ejercicio_router,
 )
-
 from presentation.routers.asiento_router import (
     router as asiento_router,
 )
 from presentation.routers.producto_router import (
     router as producto_router,
 )
-from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Sistema Contable API",
     version="1.0.0",
 )
+
+
 app.include_router(
     empresa_router,
 )
+
 app.include_router(
     cuenta_router,
 )
@@ -48,15 +50,19 @@ app.include_router(
 app.include_router(
     libro_diario_router,
 )
+
 app.include_router(
     libro_mayor_router,
 )
+
 app.include_router(
     balance_sumas_saldos_router,
 )
+
 app.include_router(
     balance_general_router,
 )
+
 app.include_router(
     ejercicio_router,
 )
@@ -64,12 +70,16 @@ app.include_router(
 app.include_router(
     estado_resultados_router,
 )
+
 app.include_router(
     asiento_router,
 )
+
 app.include_router(
     producto_router,
 )
+
+
 @app.get("/")
 def root():
 
@@ -77,27 +87,18 @@ def root():
         "mensaje": "Sistema Contable API funcionando",
     }
 
-# frontend
+
+# ============================================================
+# CORS
+# ============================================================
+
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5173",
         "http://localhost:5173",
-        "http://127.0.0.1:5500",
-
-        "http://192.168.0.9:5500",  # IP de tu laptop en casa
-        "http://192.168.3.124:5173", # IP del trabajo
-        "http://192.168.0.9:5173",
-        "http://192.168.0.13:5173",
+        "https://localhost:5173",
     ],
-   # allow_origins=["*"], # Mejor aún sirve para cualquier IP, uso sólo en desarrollo
-
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
